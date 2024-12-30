2c72a158ece7cb3e8b502e096287d2001ddbd8ec
Josef Wolf <josefwolf591@gmail.com>
2024-12-29 20:49:10 -0600

    Completed story

--- static/js/script.js
+++ static/js/script.js
@@ -36,10 +36,11 @@ class Block {
     this.restitution = 0.1; // Bounce factor (less than 1 for energy loss)
     this.friction = 0.98; // Horizontal friction
     this.isSettled = false;
+    this.color = '#32CD32';
   }
 
   draw() {
-    ctx.fillStyle = '#32CD32';
+    ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, blockSize, blockSize);
   }
 
@@ -144,10 +145,159 @@ function updateCounter() {
     updateStoryText();
   }
 
+  function updateBlockColors(amt) {
+    for (let i = 0; i < amt; i++) {
+      if (blocks[i]) {
+        blocks[i].color = '#FF0000';
+      }
+    }
+  }
+
+  function revertBlockColors(amt) {
+    for (let i = 0; i < amt; i++) {
+      if (blocks[i]) {
+        blocks[i].color = "#32CD32";
+      }
+      else {
+        break;
+      }
+    }
+  }
+
+colored = false;
 function updateStoryText() {
-  if (total > 2000000000 && total < 50000000000) {
+  mins = elapsedTime/60000;
+  if (mins < 0.1) {
+    document.getElementById('story').innerText = "";
+  }
+  else if (mins < 0.3) {
     document.getElementById('story').innerText = "Each one of these squares is worth $8.5 million.";
   }
+  else if (mins < 0.5) {
+    document.getElementById('story').innerText = "$8.5 million.";
+  }
+  else if (mins < 0.8) {
+    document.getElementById('story').innerText = "Just one of these is enough for a lifetime of luxury and generational wealth.";
+  }
+  else if (mins < 1.2) {
+    document.getElementById('story').innerText = "Based off the average annual salary and working years, the average American will only make about $3 million in their entire lifetime.";
+  }
+  else if (mins < 1.6) {
+    document.getElementById('story').innerText = "It would take almost three people a lifetime of work to get just one of these squares.";
+  }
+  else if (mins < 2) {
+    document.getElementById('story').innerText = "Musk has over 47,000 of them.";
+  }
+  else if (mins < 2.3) {
+    document.getElementById('story').innerText = "Let's explore what you could do with just a few of these."
+  }
+  else if (mins < 2.6) {
+    document.getElementById('story').innerText = "With just one of these squares, you could buy multiple luxury yachts.";
+    updateBlockColors(1);
+  }
+  else if (mins < 2.9) {
+    document.getElementById('story').innerText = "You could also buy a house in Beverly Hills.";
+  }
+  else if (mins < 3.2) {
+    document.getElementById('story').innerText = "Every four of these squares could be a brand new school for our children to attend.";
+    updateBlockColors(4);
+  }
+  else if (mins < 3.5) {
+    document.getElementById('story').innerText = "Each one of these could build multiple homeless shelters.";
+    revertBlockColors(4);
+    updateBlockColors(1);
+  }
+  else if (mins < 3.8) {
+    document.getElementById('story').innerText = "Here's how much the New York Jets are worth.";
+    updateBlockColors(635);
+  }
+  else if (mins < 4.3) {
+    document.getElementById('story').innerText = "Here's how much General Mills Inc. is worth.";
+    updateBlockColors(4200);
+  }
+  else if (mins < 4.6) {
+    document.getElementById('story').innerText = "And we aren't even close to Musk's wealth yet.";
+    revertBlockColors(4200);
+  }
+  else if (mins < 5) {
+    document.getElementById('story').innerText = "Here's how much Musk's charity gave away in 2023 according to Forbes. This much was not even enough for them to meet the minimum IRS standards.";
+    updateBlockColors(28);
+  }
+  else if (mins < 5.4) {
+    document.getElementById('story').innerText = "Musk was able to reap the tax benefits of donating while keeping much of the money he donated under his control.";
+    revertBlockColors(28);
+  }
+  else if (mins < 5.8) {
+    document.getElementById('story').innerText = "In fact, Musk spent more money backing Donald Trump in the 2024 election than his charity gave away in 2023.";
+  }
+  else if (mins < 6.1) {
+    document.getElementById('story').innerText = "CBS News estimates Musk spent $277 million to back Trump and other republicans.";
+    updateBlockColors(32);
+  }
+  else if (mins < 6.4) {
+    document.getElementById('story').innerText = "With a tiny portion of his wealth, Musk was able to buy a democratically elected government.";
+  }
+  else if (mins < 6.7) {
+    document.getElementById('story').innerText = "Now, some of you may be thinking, \"Well, he doesn't really have that much! It is all in assets!\"";
+    revertBlockColors(32);
+  }
+  else if (mins < 7.1) {
+    document.getElementById('story').innerText = "That is true, however, Musk is still able to borrow money against his assets.";
+  }
+  else if (mins < 7.4) {
+    document.getElementById('story').innerText = "In 2022, Musk borrowed over $44 billion to buy Twitter.";
+    updateBlockColors(5176);
+  }
+  else if (mins < 7.8) {
+    document.getElementById('story').innerText = "Should one person be able to outright buy an incredibly popular social media service used by officials and the general population?";
+  }
+  else if (mins < 8.2) {
+    document.getElementById('story').innerText = "Maybe you think this amount of money is disgusting for one person to have, but what can we even do about it?";
+    revertBlockColors(5176);
+  }
+  else if (mins < 8.6) {
+    document.getElementById('story').innerText = "1) You should stay informed and inform others. Knowledge is power, and if we let billionaires like Musk lie to us about their riches and power, then we have already lost.";
+  }
+  else if (mins < 9) {
+    document.getElementById('story').innerText = "2) Vote. Vote in every election you can. Learn about the candidates, who they support, what their thoughts are on billionaires like Musk, and how they want to help you.";
+  }
+  else if (mins < 9.4) {
+    document.getElementById('story').innerText = "3) Stick together. It is not the homeless guy on the corner of the street taking your money, it is billionaires like Musk who don't want to pay their taxes, raise their employees' wages, or even spend their money at all that are taking your money and hoarding it.";
+  }
+  else if (mins < 10) {
+    document.getElementById('story').innerText = "Well, we still got a long ways to go to show Musk's wealth. I'll highlight important milestones we pass the rest of the way.";
+  }
+
+
+
+
+  /*
+  if (total > 6000000000 && total < 7000000000) {
+    document.getElementById('story').innerText = "Based off the average annual salary and working years, the average American will only make about $3 million in their entire lifetime.";
+  }
+  if (total > 15000000000 && total < 16000000000) {
+    document.getElementById('story').innerText = "It would take almost three people a lifetime of work to just get one of these squares.";
+  }
+  if (total > 22000000000 && total < 23000000000) {
+    document.getElementById('story').innerText = "Elon has over 47,000 of these squares.";
+  }
+  if (total > 30000000000 && total < 31000000000) {
+    document.getElementById('story').innerText = "Let's explore what you could do with just one of these squares.";
+  }
+  if (total > 35000000000 && total < 36000000000) {
+    document.getElementById('story').innerText = "Every single one of these squares could buy multiple luxury yachts.";
+  }
+  if (total > 40000000000 && total < 41000000000) {
+    document.getElementById('story').innerText = "With just one of these, you could buy a house in Beverly Hills.";
+  }
+  if (total > 45000000000 && total < 46000000000) {
+    document.getElementById('story').innerText = "Every four of these squares could be a brand new school for our children to attend.";
+  }
+  if (total > 50000000000 && total < 51000000000) {
+    document.getElementById('story').innerText = "Each one of these could build multiple homeless shelters.";
+  }
+  */
+
 }
 
 x = 0;
